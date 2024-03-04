ac5117a51ff4ba3ab694e459c41b6265edc3639a
DQLinh <106215513+linhkburh@users.noreply.github.com>
2024-03-04 15:57:53 +0700

    x

--- WebContent/WEB-INF/views/home.jsp
+++ WebContent/WEB-INF/views/home.jsp
@@ -7,12 +7,8 @@
 <title>Home</title>
 </head>
 <body>
-	<h1>Hello world!</h1>
-
-	<P>The time on the server is ${serverTime}.</p>
-
 	<form action="user" method="post">
-		<input type="text" name="userName"><br> <input
+		 <input type="date" id="birthdaytime" name="birthdaytime"><br> <input
 			type="submit" value="Login">
 	</form>
 </body>
--- src/main/java/com/journaldev/spring/controller/HomeController.java
+++ src/main/java/com/journaldev/spring/controller/HomeController.java
@@ -1,9 +1,16 @@
 package com.journaldev.spring.controller;
 
+import java.io.InputStream;
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
@@ -11,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.journaldev.spring.model.User;
+
+import net.sf.jxls.transformer.XLSTransformer;
 
 @Controller
 public class HomeController {
@@ -32,9 +41,25 @@ public class HomeController {
 	}
 
 	@RequestMapping(value = "/user", method = RequestMethod.POST)
-	public String user(@Validated User user, Model model) {
+	public String user(@Validated User user, HttpServletResponse response, Model model) throws Exception {
 		System.out.println("User Page Requested");
 		model.addAttribute("userName", user.getUserName());
+		Map<String, Object> beans =  new HashMap<String, Object>();
+		//beans bao gồm lst ngày cùng tháng năm, tên nhân viên của Thảo quản lý, lặp qua danh sách tên do người dùng submit để tạo sheet cho từng người
+		export(response,beans);
 		return "user";
+	}
+	public void export(HttpServletResponse response, Map<String, Object> beans) throws Exception {
+		// Tao file
+		ClassLoader classLoader = getClass().getClassLoader();
+		InputStream tempFile = classLoader.getResourceAsStream("/templates/Temp_Thao.xlsx");
+		Workbook book = new XLSTransformer().transformXLS(tempFile, beans);
+		tempFile.close();
+		// Download file
+		response.setContentType("application/vnd.ms-excel");
+		//response.setHeader("Content-Disposition", "attachment;filename*=utf-8''" + common.util.EndcodeUtil.urlEncodeUtf8(templateName) + ".xlsx");
+		ServletOutputStream out = response.getOutputStream();
+		book.write(out);
+		out.close();
 	}
 }
