769bd5302ce0b249ced5d239f3850659c81f3981
DQLinh <106215513+linhkburh@users.noreply.github.com>
2024-03-05 11:00:46 +0700

    x

--- WebContent/WEB-INF/views/home.jsp
+++ WebContent/WEB-INF/views/home.jsp
@@ -7,9 +7,32 @@
 <title>Home</title>
 </head>
 <body>
-	<form action="user" method="post">
-		 <input type="date" id="birthdaytime" name="birthdaytime"><br> <input
-			type="submit" value="Login">
+	<form action="downloadExcel" method="post" id="formData">
+		 <label>Mốc thời gian: </label>
+		 <input type="date" id="dateData" name="dateData" required><br> 
+		 <label>Danh sách người được quản lý: </label><br>
+		 <div id="divStaff">
+		 <c:forEach items="#{lstStaff}" var="item">
+			<input type="text" class="" name="staffData" value="${item}"><br>
+		 </c:forEach>
+		 <input type="text" class="" name="staffData"><br>
+		 </div>
+		  <input type="submit" value="Download">
 	</form>
+	<script>
+	    function submitForm() {
+	        var formData = new FormData(document.getElementById("formData"));
+	        var xhttp = new XMLHttpRequest();
+	        xhttp.onreadystatechange = function() {
+	            if (this.readyState == 4) {
+	                if (this.status != 200) {
+	                    alert(this.responseText);
+	                }
+	            }
+	        };
+	        xhttp.open("POST", "downloadExcel", true);
+	        xhttp.send(formData);
+	    }
+    </script>
 </body>
 </html>
--- src/main/java/com/journaldev/spring/controller/HomeController.java
+++ src/main/java/com/journaldev/spring/controller/HomeController.java
@@ -1,16 +1,29 @@
 package com.journaldev.spring.controller;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
 import java.text.DateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
 import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellRangeAddress;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
@@ -29,37 +42,71 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		System.out.println("Home Page Requested, locale = " + locale);
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-
-		String formattedDate = dateFormat.format(date);
-
-		model.addAttribute("serverTime", formattedDate);
-
+		List<String> lstStaff = new ArrayList<>();
+		lstStaff.add("Hậu Phạm");
+		lstStaff.add("Nhàn");
+		lstStaff.add("Hà Mint(NA)");
+		lstStaff.add("Vinh Khoa");
+		lstStaff.add("Lương Mai");
+		lstStaff.add("Nguyễn Trang");
+		lstStaff.add("Thúy Lê");
+		lstStaff.add("Vũ Hương");
+		lstStaff.add("Thăng Nhật");
+		lstStaff.add("Ngát Mint");
+		lstStaff.add("Phương Thúy");
+		lstStaff.add("Nguyện");
+		lstStaff.add("Trang Bi");
+		lstStaff.add("Nhung Sakura");
+		lstStaff.add("Nguyễn Thủy");
+		model.addAttribute("lstStaff", lstStaff);
 		return "home";
 	}
 
-	@RequestMapping(value = "/user", method = RequestMethod.POST)
-	public String user(@Validated User user, HttpServletResponse response, Model model) throws Exception {
-		System.out.println("User Page Requested");
-		model.addAttribute("userName", user.getUserName());
+	@RequestMapping(value = "/downloadExcel", method = RequestMethod.POST)
+	public void downloadExcel(HttpServletRequest request, HttpServletResponse response, Model model) throws Exception {
 		Map<String, Object> beans =  new HashMap<String, Object>();
-		//beans bao gồm lst ngày cùng tháng năm, tên nhân viên của Thảo quản lý, lặp qua danh sách tên do người dùng submit để tạo sheet cho từng người
-		export(response,beans);
-		return "user";
-	}
-	public void export(HttpServletResponse response, Map<String, Object> beans) throws Exception {
-		// Tao file
+		String dateData = request.getParameter("dateData");
+		String[] staffData = request.getParameterValues("staffData");
+		List<String> allDayOfM = getAllDatesInMonth(dateData);
+		LocalDate date = LocalDate.parse(dateData);
+		String time = "T"+String.valueOf(date.getMonthValue())+"/"+String.valueOf(date.getYear());
 		ClassLoader classLoader = getClass().getClassLoader();
 		InputStream tempFile = classLoader.getResourceAsStream("/templates/Temp_Thao.xlsx");
+		beans.put("dayOfMonth", allDayOfM);
+		beans.put("time", time);
 		Workbook book = new XLSTransformer().transformXLS(tempFile, beans);
+		processExcel(book,staffData);
 		tempFile.close();
 		// Download file
 		response.setContentType("application/vnd.ms-excel");
-		//response.setHeader("Content-Disposition", "attachment;filename*=utf-8''" + common.util.EndcodeUtil.urlEncodeUtf8(templateName) + ".xlsx");
+		response.setHeader("Content-Disposition", "attachment;filename*=utf-8''" +time+ ".xlsx");
 		ServletOutputStream out = response.getOutputStream();
 		book.write(out);
 		out.close();
+	}
+
+	private List<String> getAllDatesInMonth(String dateString) {
+		List<String> allDatesInMonth = new ArrayList<>();
+		LocalDate date = LocalDate.parse(dateString);
+		LocalDate firstDayOfMonth = date.withDayOfMonth(1);
+		LocalDate lastDayOfMonth = date.withDayOfMonth(date.lengthOfMonth());
+		for (LocalDate d = firstDayOfMonth; !d.isAfter(lastDayOfMonth); d = d.plusDays(1)) {
+			allDatesInMonth.add(String.valueOf(d.getDayOfMonth()) + "/" + String.valueOf(d.getMonthValue()));
+		}
+		return allDatesInMonth;
+	}
+
+	private void processExcel(Workbook book, String[] staffData) {
+		Sheet sheetTemp = book.getSheet("SheetTemp");
+		for (int i = 0; i < staffData.length; i++) {
+				if(!staffData[i].equals("")) {
+				Sheet clonedSheet = book.cloneSheet(book.getSheetIndex(sheetTemp));
+				CellRangeAddress mergedRegion = new CellRangeAddress(0, 0, 1, 8);
+				clonedSheet.addMergedRegion(mergedRegion);
+				clonedSheet.getRow(0).getCell(1).setCellValue(staffData[i]);
+				book.setSheetName(book.getSheetIndex(clonedSheet), (i+1)+"."+staffData[i]);
+			}
+		}
+		book.removeSheetAt(book.getSheetIndex(sheetTemp));
 	}
 }
